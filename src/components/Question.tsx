83213b03a5e557e92ecb91d2c1e7d7ad7e3fe5dd
Nicolas Aguiar <nicolas98aguiar@gmail.com>
2021-06-24 22:41:55 -0300

    ultimo dia da NLW

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -8,15 +8,21 @@ type QuestionProps = {
         avatar: string;
     };
     children?: ReactNode;
+    isAnswered?: boolean;
+    isHighlighted?: boolean;
 }
 
 export function Question({
     content,
     author,
-    children
+    children,
+    isAnswered = false,
+    isHighlighted = false,
 }: QuestionProps) {
     return (
-        <div className="question">
+        <div className={`question
+        ${isAnswered ? 'answered': '' }
+        ${isHighlighted && !isAnswered ? 'highlighted': ''}`}>
             <p>{content}</p>
             <footer>
                 <div className="user-info">
